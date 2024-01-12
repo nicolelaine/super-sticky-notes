b156bf10bb253842d63f3e93747fd9290a64e6c7
Nicole <15011784+nicolelaine@users.noreply.github.com>
2024-01-12 14:37:59 +0100

    Update Note.js

--- src/Note.js
+++ src/Note.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Note = () => {
+const Note = (props) => {
   return (
     <li className="note">
+      {console.log(props)}
       <input className="note__title" type="text" placeholder="Title" />
       <textarea className="note__description" placeholder="Description..." />
       <span className="note__delete">X</span>
