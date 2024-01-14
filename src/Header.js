a7f3a4b711c792669c48a139e73780cadc312ef5
Nicole <15011784+nicolelaine@users.noreply.github.com>
2024-01-14 19:28:55 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-const Header = (props) => {
-  return (
-    <header>
-      <h1 className="app-header__title">Super Sticky Notes</h1>
-      <aside className="app-header__controls">
-        <button className="add-new">+ New Note</button>
-        <input
-          className="search"
-          type="text"
-          placeholder="Type here to search..."
-        />
-      </aside>
-    </header>
-  );
-};
+const Header = (props) => (
+  <header>
+    <h1 className="app-header__title">Super Sticky Notes</h1>
+    <aside className="app-header__controls">
+      <button className="add-new">+ New Note</button>
+      <input
+        className="search"
+        type="text"
+        placeholder="Type here to search..."
+        value={props.searchText}
+      />
+    </aside>
+  </header>
+);
 
 export default Header;
