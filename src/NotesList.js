22670c6ace68374a52125a7831d874a181f1ab37
Nicole <15011784+nicolelaine@users.noreply.github.com>
2024-01-14 19:29:30 +0100

    Update NotesList.js

--- src/NotesList.js
+++ src/NotesList.js
@@ -2,10 +2,11 @@ import React from "react";
 import Note from "./Note";
 
 const NotesList = (props) => {
+  const keepSearchMatches = (note) => note.doesMatchSearch;
+  const searchMatches = props.notes.filter(keepSearchMatches);
   const renderNote = (note) => <Note note={note} key={note.id} />;
-  const noteElements = props.notes.map(renderNote);
+  const noteElements = searchMatches.map(renderNote);
   return <ul className="notes-list">{noteElements}</ul>;
 };
 
 export default NotesList;
-
