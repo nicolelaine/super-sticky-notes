5672a75f76ed65394e9426eaea0bfd1733fad439
Nicole <15011784+nicolelaine@users.noreply.github.com>
2024-01-15 01:15:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,15 +28,34 @@ class App extends Component {
     this.setState({ notes: newNotes });
   };
 
+  onType = (editMeId, updatedKey, updatedValue) => {
+    //editMeId == id of the note that is edited
+    //updatedKey == title or description field
+    //updatedValue == value of title or description
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
   render() {
     return (
       <div>
         <Header addNote={this.addNote} searchText={this.state.searchText} />
-        <NotesList notes={this.state.notes} />
+        <NotesList onType={this.onType} notes={this.state.notes} />
       </div>
     );
   }
 }
 
 export default App;
-
