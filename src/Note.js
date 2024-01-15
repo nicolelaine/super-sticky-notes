efb283d3f264727bba1cd2eebad7ecceee21965f
Nicole <15011784+nicolelaine@users.noreply.github.com>
2024-01-15 01:15:38 +0100

    Update Note.js

--- src/Note.js
+++ src/Note.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-const Note = (props) => (
-  <li className="note">
-    <input
-      type="text"
-      placeholder="Title"
-      className="note__title"
-      value={props.note.title}
-    />
-    <textarea
-      placeholder="Description..."
-      className="note__description"
-      value={props.note.description}
-    />
-    <span className="note__delete">X</span>
-  </li>
-);
+const Note = (props) => {
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+  return (
+    <li className="note">
+      <input
+        type="text"
+        placeholder="Title"
+        className="note__title"
+        value={props.note.title}
+        onChange={updateTitle}
+      />
+      <textarea
+        placeholder="Description..."
+        className="note__description"
+        value={props.note.description}
+        onChange={updateDescription}
+      />
+      <span className="note__delete">X</span>
+    </li>
+  );
+};
 
 export default Note;
